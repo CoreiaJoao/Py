9bd58247b2282e414658c061472a590763b57fe2
João Monteiro <jfcm98647@gmail.com>
2022-07-17 15:39:17 +0100

    Roller Coaster exercise

---
+++ Third Day/thirdDay.py
@@ -0,0 +1,6 @@
+print("Welcome to the rollercoaster!!")
+height= int(input("What is your heigth in cm?"))
+if height>120 : 
+    print("You can ride the rollercoaster!")
+else:
+    print("You are to short to ride the rollercoaster 😓")
